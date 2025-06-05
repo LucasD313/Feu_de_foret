e7b60d71bb3f4cff4e67353b806c7d818de89cce
Thomas <Thomas51soares@gmail.com>
2025-06-05 17:59:51 +0200

    Ajout de la classe Simulateur avec génération de carte, propagation du feu et export HTML

---
+++ feu_foret/simulateur.py
@@ -0,0 +1,93 @@
+import random
+import copy
+
+class Simulateur:
+    VIDE = '.'
+    ARBRE = 'A'
+    EAU = 'E'
+    FEU = 'F'
+    BRULE = 'B'
+
+    def __init__(self, largeur, hauteur, pourcentage_arbres):
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.pourcentage_arbres = pourcentage_arbres
+        self.carte = self.generer_carte()
+
+    def generer_carte(self):
+        carte = []
+        for y in range(self.hauteur):
+            ligne = []
+            for x in range(self.largeur):
+                r = random.random()
+                if r < self.pourcentage_arbres:
+                    ligne.append(self.ARBRE)
+                else:
+                    ligne.append(self.VIDE)
+            carte.append(ligne)
+        return carte
+
+    def propager_feu(self, x, y):
+        if self.carte[y][x] != self.ARBRE:
+            return
+
+        directions = [(-1, -1), (-1, 0), (-1, 1),
+                      (0, -1),         (0, 1),
+                      (1, -1),  (1, 0), (1, 1)]
+
+        def feu_recursive(x, y):
+            if not (0 <= x < self.largeur and 0 <= y < self.hauteur):
+                return
+            if self.carte[y][x] != self.ARBRE:
+                return
+            self.carte[y][x] = self.BRULE
+            for dx, dy in directions:
+                feu_recursive(x + dx, y + dy)
+
+        feu_recursive(x, y)
+
+    def trouver_meilleur_deboisement(self, x, y):
+        arbres = [(i, j) for j in range(self.hauteur)
+                         for i in range(self.largeur)
+                         if self.carte[j][i] == self.ARBRE]
+
+        min_brulure = float('inf')
+        meilleur = None
+
+        for i, j in arbres:
+            copie = copy.deepcopy(self.carte)
+            copie[j][i] = self.VIDE
+            sim_temp = Simulateur(self.largeur, self.hauteur, self.pourcentage_arbres)
+            sim_temp.carte = copie
+            sim_temp.propager_feu(x, y)
+
+            brulees = sum(row.count(self.BRULE) for row in sim_temp.carte)
+            if brulees < min_brulure:
+                min_brulure = brulees
+                meilleur = (i, j)
+
+        return meilleur
+
+    def exporter_html(self, nom_fichier="carte.html"):
+        couleurs = {
+            self.VIDE: "#c2b280",
+            self.ARBRE: "#228B22",
+            self.EAU: "#1E90FF",
+            self.BRULE: "#8B0000",
+        }
+
+        html = "<html><head><style>"
+        html += "table {border-collapse: collapse;} td {width: 20px; height: 20px;}"
+        html += "</style></head><body><table>"
+
+        for ligne in self.carte:
+            html += "<tr>"
+            for case in ligne:
+                couleur = couleurs.get(case, "#000000")
+                html += f'<td style="background-color:{couleur}"></td>'
+            html += "</tr>"
+
+        html += "</table></body></html>"
+
+        with open(nom_fichier, "w") as f:
+            f.write(html)
